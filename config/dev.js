bfe7c5bd0320a91544c697bf928d9b7ab6da3397
GordonWoon <gordonwoon@live.com>
2019-11-11 00:49:35 +0800

    config dev format

--- config/dev.js
+++ config/dev.js
@@ -1,4 +1,3 @@
 module.exports = {
-  mongoURI:
-    'mongodb+srv://developer:QXw7TKqu74JgnkC8Kb@cluster0-oh4ob.mongodb.net/test?retryWrites=true&w=majority'
+  mongoURI: 'mongodb+srv://developer:QXw7TKqu74JgnkC8Kb@cluster0-oh4ob.mongodb.net/test?retryWrites=true&w=majority'
 };
